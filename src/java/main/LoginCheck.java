38adce5152c2320c7888eacbdfb5d6477e509e97
donch <donch@LAPTOP-3CTCCR2M>
2018-04-12 22:47:34 +0900

    OC　H29年度版

--- src/java/main/LoginCheck.java
+++ src/java/main/LoginCheck.java
@@ -7,21 +7,13 @@ package main;
 
 import beans.UserBean;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import sql.SqlMain;
 
 /**
  *
